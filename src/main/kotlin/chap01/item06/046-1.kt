3d6bed2a7dbfd91b94321d852e37983840fd11fe
Soonho Hwang <20142093@edu.hanbat.ac.kr>
2022-05-10 10:27:56 +0900

    add file in item06 & item06 done

---
+++ src/main/kotlin/chap01/item06/046-1.kt
@@ -0,0 +1,10 @@
+package chap01.item06
+
+//inline fun <reified T> String.readObject(): T {
+//    //...
+//    if (incorrectSign) {
+//        throw JsonParsingException()
+//    }
+//    //...
+//    return result
+//}
